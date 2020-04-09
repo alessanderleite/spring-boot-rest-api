ad5b33135a53ff76d73bccb86e5ef95cccba0fa0
Alessander <alessander.logistica@gmail.com>
2020-04-09 12:38:46 -0300

    create the model class for User

---
+++ src/main/java/br/com/alessanderleite/app/model/User.java
@@ -0,0 +1,75 @@
+package br.com.alessanderleite.app.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class User implements Serializable {
+	private static final long serialVersionUID = -2856606861295949394L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "password", nullable = false)
+	private String password;
+	
+	@Column(name = "name", nullable = false)
+	private String name;
+	
+	@Column(name = "email", nullable = false)
+	private String email;
+
+	public User() {}
+
+	public User(String password, String name, String email) {
+		this.password = password;
+		this.name = name;
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", password=" + password + ", name=" + name + ", email=" + email + "]";
+	}
+	
+}
